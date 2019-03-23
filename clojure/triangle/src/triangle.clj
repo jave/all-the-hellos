02ea55d93398561fdcbf5473a756f04a12e5ede2
joakim <joakim@verona.se>
2019-03-23 13:06:59 +0100

    triangle clojure exercism

--- clojure/triangle/src/triangle.clj
+++ clojure/triangle/src/triangle.clj
@@ -1,5 +1,15 @@
 (ns triangle)
 
-(defn type [] ;; <- arglist goes here
-      ;; your code goes here
+(defn type [a b c] ;; <- arglist goes here
+  ;; your code goes here
+  (let [setcount (count(set (list a b c)))
+        sorted  (sort (list a b c))]
+    (cond
+      (<= (+ (first sorted) (second sorted)) (nth sorted 2)) :illogical
+      (= 1 setcount) :equilateral
+      (= 2 setcount) :isosceles
+      (= 3 setcount) :scalene
+      
+      ))
       )
+;;(type 1 2 1)
