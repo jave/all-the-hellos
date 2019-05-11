33d58df1c523954c45772ba14ba78c10ca805073
joakim <joakim@verona.se>
2019-05-11 23:36:58 +0200

    removed duplicates

--- ocaml/leap-2/leap.ml
+++
@@ -1,6 +0,0 @@
-let is_divisable y x = (y mod x) = 0 ;;
-let leap_year year =     is_divisable year 4 &&
-                           not ( is_divisable year 100)
-                         || is_divisable year 400;;
-
-
