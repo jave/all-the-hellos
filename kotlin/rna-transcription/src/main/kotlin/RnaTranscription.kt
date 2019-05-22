ae87b4507a5c782e3afb4402839de3bda83fc878
joakim <joakim@verona.se>
2019-05-22 20:07:53 +0200

    kotlin rnatranscript exercism

--- kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -1,6 +1,23 @@
-fun transcribeToRna(dna: String):{
-        
-        String = ""
-}
+// for some reason the tests didnt work well for this one.
+// but i verified in a repl.
 
+val dnaToRna = mapOf('G' to 'C', 'C' to 'G', 'T'  to 'A', 'A' to 'U')
+
+//'it' anaphoric ref
+fun transcribeToRna(dna: String) = dna.map {dnaToRna[it]}.joinToString("")
+
+//another way to do the mapping
+
+// fun mapnuc (c:Char):Char{
+//         return when (c){
+//                 'A' -> 'U'
+//                 'G' -> 'C'
+//                 'C' -> 'G'
+//                 'T' -> 'A'
+//                 else -> c
+//         }
+// }
+
+        
+//  fun transcribeToRna(dna: String) = dna.map({c:Char -> mapnuc(c)}).joinToString("")
 
--- kotlin/rna-transcription/src/test/kotlin/RnaTranscriptionTest.kt
+++ kotlin/rna-transcription/src/test/kotlin/RnaTranscriptionTest.kt
@@ -18,24 +18,28 @@ class RnaTranscriptionTest {
         assertEquals("G", transcribeToRna("C"))
     }
 
+    
     @Test
     fun guanineComplementIsCytosine() {
         assertEquals("C", transcribeToRna("G"))
     }
 
+    
     @Test
     fun thymineComplementIsAdenine() {
         assertEquals("A", transcribeToRna("T"))
     }
 
+    
     @Test
     fun adenineComplementIsUracil() {
         assertEquals("U", transcribeToRna("A"))
     }
 
+    
     @Test
     fun rnaTranscription() {
-        assertEquals("UGCACCAGAAUU", transcribeToRna("ACGTGGTCTTAA"))
+        assertEquals("UGCACCAGAAUU", transcribeToRna("ACGTGGTCTTAA")).joinToString()
     }
 
 }
